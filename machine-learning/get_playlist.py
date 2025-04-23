3f0a61d9aaa58f5a45bd62f7d8010471def974e9
gabriella-codrington <gac4022@gmail.com>
2025-04-23 00:01:07 -0400

    merge, get_playlist

--- machine-learning/get_playlist.py
+++ machine-learning/get_playlist.py
@@ -1,7 +1,7 @@
 import json
-from dotenv import load_dotenv
-import os
 import base64
+import os
+from dotenv import load_dotenv
 from requests import post, get
 
 load_dotenv()
@@ -9,26 +9,27 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+
 def get_token():
-    auth_string = client_id + ":" + client_secret
+    auth_string = f"{client_id}:{client_secret}"
     auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
 
     url = "https://accounts.spotify.com/api/token"
     headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Authorization": f"Basic {auth_base64}",
+        "Content-Type": "application/x-www-form-urlencoded",
     }
     data = {"grant_type": "client_credentials"}
     result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
+    result.raise_for_status()
+    token = result.json()["access_token"]
     return token
 
-token = get_token()
 
 def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+    return {"Authorization": f"Bearer {token}"}
+
 
 def get_playlist_uri(token, emotion):
     url = "https://api.spotify.com/v1/search"
@@ -40,7 +41,6 @@ def get_playlist_uri(token, emotion):
     playlists = result.json()["playlists"]["items"]
 
     if playlists:
-        return playlists[0]["uri"]  
+        return playlists[0]["uri"]
     else:
         return None
-
